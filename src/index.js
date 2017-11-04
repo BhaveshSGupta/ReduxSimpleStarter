38b39eda57c2dda30799a96959ad1bb177c76b9a
Bhavesh S. Gupta <BhaveshSGupta@gmail.com>
2017-11-04 22:43:35 +0530

    007-ES6 Import Statements

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
+import React from 'react';
+
 // Create a new component. This should produce some HTML
 const App = function() {
   return <div> hi! </div>;
 }
 // Take this component's generated HTML and put it  on the page
+React.render(App);
