830762ea2854f012cc8303338c68aba7100659e1
Bhavesh S. Gupta <BhaveshSGupta@gmail.com>
2017-11-05 00:31:15 +0530

    014-Class-Based Components converted code from functional based to class based. And yes should I say that after working in investis for like 2 years now I can find double spaces from text :(, First line imports reacts and create variable as component having react.component

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,7 +1,14 @@
-import React from 'react';
+import React,{ Component } from 'react';
 
-const SearchBar = () =>{
-  return <input />;
-};
+// Below is functional type of component
+// const SearchBar = () =>{
+//   return <input />;
+// };
+// Above is functional type of component
 
+class SearchBar extends Component {
+  render(){
+    return <input />;
+  }
+}
 export default SearchBar;
