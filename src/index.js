f5095d150d69b4ecb298109443e016ca4813772a
Bhavesh S. Gupta <BhaveshSGupta@gmail.com>
2017-11-05 02:36:28 +0530

    020-Youtube Search Response

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import YTSearch from 'youtube-api-search'
+
 import SearchBar from './components/search_bar';
 
 const API_KEY='';
+
+YTSearch({key:API_KEY,term:'aaj jane ki ziid'},function(data) {
+  console.log(data);
+})
 
 // Create a new component. This should produce some HTML
 const App = function() {
