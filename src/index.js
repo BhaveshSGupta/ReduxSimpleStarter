28b6556be6caa3bc2cab737b2f50119d1f5ce051
Bhavesh S. Gupta <BhaveshSGupta@gmail.com>
2017-11-05 12:01:17 +0530

    027-Handling Null Props

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDom from 'react-dom';
 import YTSearch from 'youtube-api-search'
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 const API_KEY='';
 
 // Create a new component. This should produce some HTML
@@ -11,7 +12,7 @@ class App extends Component{
     super(props);
     this.state={videos:[]};
     YTSearch({key:API_KEY,term:'aaj jane ki ziid'}, (videos)=> {
-      console.log(videos);
+      // console.log(videos);
       this.setState({videos});
     });
   }
@@ -19,6 +20,7 @@ class App extends Component{
     return (
       <div>
         <SearchBar />
+        <VideoDetail video={this.state.videos[0]}/>
         <VideoList videos={this.state.videos} />
       </div>
     );
