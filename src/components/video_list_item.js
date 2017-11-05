bb14c369d98de6e1eb96c84af3ed69d98add13a0
Bhavesh S. Gupta <BhaveshSGupta@gmail.com>
2017-11-05 12:15:53 +0530

    028-Video Selection

--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
-const VideoListItem = ({video})=> {
+const VideoListItem = ({video,onVideoSelect})=> {
   //const video=props.video;
   const imgUrl=video.snippet.thumbnails.default.url;
   const title=video.snippet.title;
   return (
-    <li className="list-group-item">
+    <li className="list-group-item" onClick={()=>onVideoSelect(video)}>
       <div className="video-list media">
         <div className="media-left">
           <img src={imgUrl} className="media-object"/>
